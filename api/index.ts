961a0e8e2b8659d72766d5a83b5243a5d3c976eb
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-02-12 21:50:26 +0600

    add route in index.ts

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,7 @@ import config from "./config";
 import artistsRouter from "./routes/artists";
 import albumsRouter from "./routes/albums";
 import tracksRouter from "./routes/tracks";
+import usersRouter from "./routes/users";
 
 const app = express();
 const port = 8000;
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
 app.use('/tracks', tracksRouter);
+app.use('/users', usersRouter);
 
 const run = async () => {
   await mongoose.connect(config.mongoose.db);
