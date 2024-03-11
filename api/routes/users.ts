b6842d6290dca6a2a43c0f111840f714ad658384
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-03-11 23:15:10 +0600

    #91: test husky

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -56,6 +56,7 @@ usersRouter.post('/google', async (req, res, next) => {
     });
 
     const payload = ticket.getPayload();
+
     if (!payload) {
       return res.status(400).send({ error: 'Google login is wrong!' });
     }
